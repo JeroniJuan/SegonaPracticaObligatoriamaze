22e9336e224b553f9cbb832e6ea7136b1ef6cc7e
Jeroni Juan <jjuanh@esliceu.com>
2024-11-21 15:19:55 +0100

    21/11, funcions de totes las pareds en js

--- src/main/resources/static/js/map.js
+++ src/main/resources/static/js/map.js
@@ -1,87 +1,127 @@
 const canvas = document.getElementById('roomCanvas');
-        const ctx = canvas.getContext('2d');
-        const thickness = 8;
-        const margin = 50;
-        const roomSize = canvas.width - margin * 2;
+const ctx = canvas.getContext('2d');
+const thickness = 8;
+const margin = 50;
+const roomSize = canvas.width - margin * 2;
 
-        function drawWallNormal(x1, y1, x2, y2) {
-            ctx.lineWidth = thickness;
-            ctx.strokeStyle = "black";
-            ctx.beginPath();
-            ctx.moveTo(x1, y1);
-            ctx.lineTo(x2, y2);
-            ctx.stroke();
-        }
+function drawWallNorthNormal() {
+    drawWallNormal(margin, margin, margin + roomSize, margin);
+}
 
-        function drawWallWithDoor(x1, y1, x2, y2, doorSize = 50) {
-            const totalLength = Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
-            const startLength = (totalLength - doorSize) / 2;
+function drawWallSouthNormal() {
+    drawWallNormal(margin, margin + roomSize, margin + roomSize, margin + roomSize);
+}
 
-            const part1X = x1 + ((x2 - x1) * startLength) / totalLength;
-            const part1Y = y1 + ((y2 - y1) * startLength) / totalLength;
+function drawWallEastNormal() {
+    drawWallNormal(margin + roomSize, margin, margin + roomSize, margin + roomSize);
+}
 
+function drawWallWestNormal() {
+    drawWallNormal(margin, margin, margin, margin + roomSize);
+}
 
-            const part2X = x2 - ((x2 - x1) * startLength) / totalLength;
-            const part2Y = y2 - ((y2 - y1) * startLength) / totalLength;
+function drawWallNorthOpen() {
+    drawWallWithDoor(margin, margin, margin + roomSize, margin);
+}
 
-            ctx.lineWidth = thickness;
-            ctx.strokeStyle = "black";
+function drawWallSouthOpen() {
+    drawWallWithDoor(margin, margin + roomSize, margin + roomSize, margin + roomSize);
+}
 
-            ctx.beginPath();
-            ctx.moveTo(x1, y1);
-            ctx.lineTo(part1X, part1Y);
-            ctx.stroke();
+function drawWallEastOpen() {
+    drawWallWithDoor(margin + roomSize, margin, margin + roomSize, margin + roomSize);
+}
 
-            ctx.beginPath();
-            ctx.moveTo(part2X, part2Y);
-            ctx.lineTo(x2, y2);
-            ctx.stroke();
-        }
+function drawWallWestOpen() {
+    drawWallWithDoor(margin, margin, margin, margin + roomSize);
+}
 
-        function drawWallWithClosedDoor(x1, y1, x2, y2, doorSize = 50) {
-            const totalLength = Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
-            const startLength = (totalLength - doorSize) / 2;
+function drawWallNorthClosed() {
+    drawWallWithClosedDoor(margin, margin, margin + roomSize, margin);
+}
 
-            const part1X = x1 + ((x2 - x1) * startLength) / totalLength;
-            const part1Y = y1 + ((y2 - y1) * startLength) / totalLength;
+function drawWallSouthClosed() {
+    drawWallWithClosedDoor(margin, margin + roomSize, margin + roomSize, margin + roomSize);
+}
 
-            const part2X = x2 - ((x2 - x1) * startLength) / totalLength;
-            const part2Y = y2 - ((y2 - y1) * startLength) / totalLength;
+function drawWallEastClosed() {
+    drawWallWithClosedDoor(margin + roomSize, margin, margin + roomSize, margin + roomSize);
+}
 
-            ctx.lineWidth = thickness;
+function drawWallWestClosed() {
+    drawWallWithClosedDoor(margin, margin, margin, margin + roomSize);
+}
 
-            ctx.strokeStyle = "black";
-            ctx.beginPath();
-            ctx.moveTo(x1, y1);
-            ctx.lineTo(part1X, part1Y);
-            ctx.stroke();
+function drawWallNormal(x1, y1, x2, y2) {
+    ctx.lineWidth = thickness;
+    ctx.strokeStyle = "black";
+    ctx.beginPath();
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.stroke();
+}
 
-            ctx.beginPath();
-            ctx.moveTo(part2X, part2Y);
-            ctx.lineTo(x2, y2);
-            ctx.stroke();
+function drawWallWithDoor(x1, y1, x2, y2, doorSize = 50) {
+    const totalLength = Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
+    const startLength = (totalLength - doorSize) / 2;
 
-            ctx.strokeStyle = "brown";
-            ctx.beginPath();
-            ctx.moveTo(part1X, part1Y);
-            ctx.lineTo(part2X, part2Y);
-            ctx.stroke();
-        }
+    const part1X = x1 + ((x2 - x1) * startLength) / totalLength;
+    const part1Y = y1 + ((y2 - y1) * startLength) / totalLength;
 
-        function drawRoom() {
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
+    const part2X = x2 - ((x2 - x1) * startLength) / totalLength;
+    const part2Y = y2 - ((y2 - y1) * startLength) / totalLength;
 
-            // Pared superior (normal)
-            drawWallNormal(margin, margin, margin + roomSize, margin);
+    ctx.lineWidth = thickness;
+    ctx.strokeStyle = "black";
 
-            // Pared derecha (con puerta abierta)
-            drawWallWithDoor(margin + roomSize, margin, margin + roomSize, margin + roomSize);
+    ctx.beginPath();
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(part1X, part1Y);
+    ctx.stroke();
 
-            // Pared inferior (con puerta cerrada)
-            drawWallWithClosedDoor(margin, margin + roomSize, margin + roomSize, margin + roomSize);
+    ctx.beginPath();
+    ctx.moveTo(part2X, part2Y);
+    ctx.lineTo(x2, y2);
+    ctx.stroke();
+}
 
-            // Pared izquierda (normal)
-            drawWallNormal(margin, margin, margin, margin + roomSize);
-        }
+function drawWallWithClosedDoor(x1, y1, x2, y2, doorSize = 50) {
+    const totalLength = Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
+    const startLength = (totalLength - doorSize) / 2;
 
-        drawRoom();
+    const part1X = x1 + ((x2 - x1) * startLength) / totalLength;
+    const part1Y = y1 + ((y2 - y1) * startLength) / totalLength;
+
+    const part2X = x2 - ((x2 - x1) * startLength) / totalLength;
+    const part2Y = y2 - ((y2 - y1) * startLength) / totalLength;
+
+    ctx.lineWidth = thickness;
+
+    ctx.strokeStyle = "black";
+    ctx.beginPath();
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(part1X, part1Y);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(part2X, part2Y);
+    ctx.lineTo(x2, y2);
+    ctx.stroke();
+
+    ctx.strokeStyle = "brown";
+    ctx.beginPath();
+    ctx.moveTo(part1X, part1Y);
+    ctx.lineTo(part2X, part2Y);
+    ctx.stroke();
+}
+
+function drawRoom() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    drawWallNorthNormal();
+    drawWallSouthOpen();
+    drawWallEastClosed();
+    drawWallWestNormal();
+}
+
+drawRoom();
